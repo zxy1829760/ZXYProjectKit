68a88b89893ab4c4dffefde08968ee75659de8f8
zhangxiaoyang <zhangxiaoyang@Bailun.com>
2021-02-22 19:55:01 +0800

    测试

--- ZXYProjectKit/ViewController.swift
+++ ZXYProjectKit/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.title = "项目功能库开始"
     }
 
 
